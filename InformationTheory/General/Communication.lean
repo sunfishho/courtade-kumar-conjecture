e8977f5aed0ef801b5486984db8b8acf93fd6fd2
Matthew Ho <sunfishho12@gmail.com>
2026-02-15 02:55:48 -0800

    add statement of shannon's noisy-channel theorem

--- InformationTheory/General/Communication.lean
+++ InformationTheory/General/Communication.lean
@@ -1,8 +1,8 @@
 import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
-import InformationTheory.Discrete.DiscreteRandomVariable
 import InformationTheory.General.RandomVariable
-import InformationTheory.Discrete.DiscreteInformationQuantities
 import Mathlib.Probability.Kernel.Defs
+import Mathlib.Analysis.SpecialFunctions.Log.Base
+import InformationTheory.General.InformationQuantities
 
 /- Goals of this file:
  - Define a discrete channel
@@ -10,8 +10,7 @@ import Mathlib.Probability.Kernel.Defs
  - State the Shannon capacity theorem (sorryed)
  -/
 
-open ProbabilityTheory
-open MeasureTheory
+open ProbabilityTheory MeasureTheory InformationQuantities
 
 structure StationaryMemorylessChannel (Ω₁ Ω₂ : Type*)
 [MeasurableSpace Ω₁] [MeasurableSpace Ω₂] where
@@ -20,7 +19,18 @@ structure StationaryMemorylessChannel (Ω₁ Ω₂ : Type*)
 
 namespace MemorylessChannel
 
-variable {Ω₁ Ω₂ : Type*} [MeasurableSpace Ω₁] [MeasurableSpace Ω₂]
+variable {Ω₁ Ω₂ : Type*} [MeasurableSpace Ω₁] [MeasurableSpace Ω₂] (N : ℕ+) (M : ℕ+)
+
+/-- Used for error probability calculation. -/
+noncomputable def uniform_input_measure (M : ℕ+) : ProbabilityMeasure (Fin M) :=
+  ⟨uniformOn (Set.univ : Set (Fin M)),
+  by
+     classical
+     -- uniformOn_isProbabilityMeasure has been deprecated in lean 2.7.0
+     exact ProbabilityTheory.uniformOn_isProbabilityMeasure
+       (s := (Set.univ : Set (Fin M)))
+       (hs  := (Set.finite_univ : Set.Finite (Set.univ : Set (Fin M))))
+       (hs' := (Set.univ_nonempty : Set.Nonempty (Set.univ : Set (Fin M))))⟩
 
 -- Produces output measure upon applying a channel on input measure
 noncomputable def apply_channel_to_measure (C : StationaryMemorylessChannel Ω₁ Ω₂) (μ : Measure Ω₁)
@@ -28,10 +38,10 @@ noncomputable def apply_channel_to_measure (C : StationaryMemorylessChannel Ω�
   μ.bind C.K
 
 -- Produces codewords given message from [M]. Deterministic.
-def encoder (Ω : Type*) (N : ℕ+) (M : ℕ+) := Fin M → Vector Ω N
+def encoder (Ω : Type*) (N : ℕ+) (M : ℕ+) := Fin M → Fin N → Ω
 
 -- Produces codeword given output of channel. Deterministic.
-def decoder (Ω : Type*) (N : ℕ+) (M : ℕ+) := Vector Ω N → Fin M
+def decoder (Ω : Type*) (N : ℕ+) (M : ℕ+) := (Fin N → Ω) → Fin M
 
 /- The encoder-decoder pair are encoded in this struct,
 which also encodes blocklength (N) and number of possible messages (M) -/
@@ -41,15 +51,76 @@ structure CodingScheme (C : StationaryMemorylessChannel Ω₁ Ω₂) where
   encoder : encoder Ω₁ N M
   decoder : decoder Ω₂ N M
 
--- Calculate the rate of a coding scheme as log_2 M/N
+/-- Calculate the rate of a coding scheme as log_2 M/N -/
 noncomputable def rate (C : StationaryMemorylessChannel Ω₁ Ω₂)
 (CS : CodingScheme C) : ℝ :=
   (Real.logb 2 CS.M) / CS.N
 
--- Calculate the average error probability assuming a uniform distribution on input messages
-noncomputable def avg_error_prob (C : StationaryMemorylessChannel Ω₁ Ω₂)
-(CS : CodingScheme C) : ℝ :=
+/-- Create output block from product of independent kernels -/
+noncomputable def blockOutputMeasure
+    (K : Kernel Ω₁ Ω₂) (N : ℕ) (x : Fin N → Ω₁) :
+    Measure (Fin N → Ω₂) :=
+by
+  classical
+  exact Measure.pi (fun i : Fin N => K (x i))
+
+/-- Calculate the average error probability of a coding scheme -/
+noncomputable def error_probability (C : StationaryMemorylessChannel Ω₁ Ω₂)
+(CS : CodingScheme C) : ℝ := by
+  classical
+  let M := CS.M
+  let N := CS.N
+  let μ := uniform_input_measure M
+  let codeword_error_prob : Fin M → ℝ := fun m =>
+    let x := CS.encoder m
+    let output_distribution := blockOutputMeasure C.K N x
+    let x_hat := output_distribution.map CS.decoder
+    1 - (x_hat {m}).toReal
+  exact (1 / M : ℝ) * (∑ m : Fin M, codeword_error_prob m)
+
+def achievable_M (C : StationaryMemorylessChannel Ω₁ Ω₂) (N : ℕ+) (ε : ℝ) (M : ℕ+) : Prop :=
+  ∃ CS : CodingScheme C, error_probability C CS ≤ ε ∧ CS.N = N ∧ CS.M = M
+
+/-- defining M*(n,ε) -/
+noncomputable def M_avg
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) (N : ℕ+) (ε : ℝ) : WithTop ℕ := by
+  classical
+  exact sSup { M : WithTop ℕ | ∃ M : ℕ+, achievable_M C N ε M }
+
+/-- defining R*(n,ε) -/
+noncomputable def R_star
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) (N : ℕ+) (ε : ℝ) : WithTop ℝ :=
+by
+  classical
+  match M_avg (Ω₁:=Ω₁) (Ω₂:=Ω₂) C N ε with
+  | ⊤ => exact ⊤
+  | (M : ℕ) =>
+      exact (((Real.logb 2 (M : ℝ)) / (N : ℝ) : ℝ) : WithTop ℝ)
+
+noncomputable def R_epsilon
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) (ε : ℝ) : WithTop ℝ :=
+  Filter.limsup (fun N : ℕ => R_star (Ω₁ := Ω₁) (Ω₂ := Ω₂) C (⟨N + 1, Nat.succ_pos _⟩) ε)
+    Filter.atTop
+
+noncomputable def shannon_capacity
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) : WithTop ℝ :=
+  ⨅ (ε : {ε : ℝ // 0 < ε}), R_epsilon (Ω₁ := Ω₁) (Ω₂ := Ω₂) C ε.1
+
+noncomputable def information_capacity
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) : WithTop ℝ :=
+by
+  classical
+  let joint_μ (μ : ProbabilityMeasure Ω₁) : Measure (Ω₁ × Ω₂) :=
+    (μ.toMeasure).compProd C.K
+  let ENNReal_to_WithTopReal (d : ENNReal) : WithTop ℝ :=
+    if d = ⊤ then ⊤ else (d.toReal : WithTop ℝ)
+  exact sSup ((fun μ : ProbabilityMeasure Ω₁ =>
+    ENNReal_to_WithTopReal (mutual_information (joint_μ μ))) '' Set.univ)
+
+theorem shannon_capacity_theorem
+  (C : StationaryMemorylessChannel Ω₁ Ω₂) :
+  information_capacity (Ω₁ := Ω₁) (Ω₂ := Ω₂) C = shannon_capacity (Ω₁ := Ω₁) (Ω₂ := Ω₂) C :=
+by
+  sorry
 
 end MemorylessChannel
-
--- noncomputable def ShannonCapacity {Ω₁ Ω₂ : Type*}
--- InformationTheory/General/InformationQuantities.lean
+++ InformationTheory/General/InformationQuantities.lean
@@ -1,13 +1,24 @@
 import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
 import InformationTheory.Discrete.DiscreteRandomVariable
 import InformationTheory.General.RandomVariable
-import InformationTheory.Discrete.DiscreteInformationQuantities
 import Mathlib.Probability.Kernel.Defs
+import Mathlib.InformationTheory.KullbackLeibler.Basic
 
 import Mathlib
 
+open ProbabilityTheory
+open MeasureTheory
+open scoped ENNReal
+
 namespace InformationQuantities
 
+variable {Ω₁ Ω₂ : Type*} [MeasurableSpace Ω₁] [MeasurableSpace Ω₂]
+
 -- I(P_X, P_{Y|X}) = D(P_{X,Y}||P_XP_Y)
-noncomputable def mutual_information
-  {Ω: Type u}
+-- should probably take in probability measures instead of measures
+noncomputable def mutual_information (PQ : Measure (Ω₁ × Ω₂)) : ℝ≥0∞ :=
+  let P := PQ.fst
+  let Q := PQ.snd
+  InformationTheory.klDiv PQ (P.prod Q)
+
+end InformationQuantities
