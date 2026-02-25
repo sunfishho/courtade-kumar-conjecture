5fdc3b0f864e0feb0c12894ff9f1232d7e133fe0
Matthew Ho <sunfishho12@gmail.com>
2026-02-25 14:03:14 -0800

    convert kl to base 2 in nonnegativity statement

--- InformationTheory/Exercises/ch2_yp_yw/ypyw_theorem_2_3.lean
+++ InformationTheory/Exercises/ch2_yp_yw/ypyw_theorem_2_3.lean
@@ -1,9 +1,10 @@
 import Mathlib
+import InformationTheory.General.InformationQuantities
 
 -- Theorem 2.3 in Information Theory: From Coding to Learning
 
-open InformationTheory MeasureTheory
+open MeasureTheory InformationQuantities
 
 theorem nonnegativity_kl_divergence {Ω : Type*} [MeasureSpace Ω] [StandardBorelSpace Ω] (P Q : Measure Ω) [IsProbabilityMeasure P] [IsProbabilityMeasure Q] :
-  klDiv P Q ≥ 0 := by
+  klDivBase2 P Q ≥ 0 := by
   sorry
