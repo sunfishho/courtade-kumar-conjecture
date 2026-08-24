de9365c6979d4a725d6fdee4f688d2c67d8c9c35
Matthew Ho <sunfishho12@gmail.com>
2026-08-24 03:20:07 -0400

    Import exact TOP Bernstein coefficient certificate

--- InformationTheory.lean
+++ InformationTheory.lean
@@ -32,6 +32,7 @@ import InformationTheory.CourtadeKumar.CapAuxDerivative
 import InformationTheory.CourtadeKumar.CapAuxConcavity
 import InformationTheory.CourtadeKumar.CapClosure
 import InformationTheory.CourtadeKumar.BernsteinCertificate
+import InformationTheory.CourtadeKumar.TopCertificateData
 import InformationTheory.CourtadeKumar.EntropyDropTransition
 import InformationTheory.CourtadeKumar.TopEndpointIdentity
 import InformationTheory.CourtadeKumar.ReserveConcavity
